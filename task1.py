f407bad7ee169fa26b01a4c1d5d0fbeaa7a03405
unknown <vafin-2005@list.ru>
2021-08-28 22:09:29 +0300

    task

---
+++ task1.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+def motion():
+    c.move(ball, 1, 0)
+    if c.coords(ball)[2] < 300:
+        root.after(10, motion)
+
+root = Tk()
+c = Canvas(root, width=300, height=200,
+           bg="white")
+c.pack()
+ball = c.create_oval(0, 100, 40, 140,
+                     fill='green')
+motion()
+root.mainloop()
